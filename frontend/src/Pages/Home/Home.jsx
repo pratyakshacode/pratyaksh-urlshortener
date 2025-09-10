f6c99e54fcb4259655c0cc526a8a9bd70497f35a
pratyakshacode <pratyaksh@beingzero.in>
2025-09-10 11:54:03 +0530

    second commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Pratyaksh Url Shortener
           </Title>
 
           <Text
